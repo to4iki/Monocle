87d257518dddca3367efe722d6966484c5102d0d
tsk takezawa <tsk.take815@gmail.com>
2015-12-20 16:22:24 +0900

    Initial Commit

---
+++ Monocle/Monocle.h
@@ -0,0 +1,19 @@
+//
+//  Monocle.h
+//  Monocle
+//
+//  Created by to4iki on 12/20/15.
+//  Copyright © 2015 to4iki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Monocle.
+FOUNDATION_EXPORT double MonocleVersionNumber;
+
+//! Project version string for Monocle.
+FOUNDATION_EXPORT const unsigned char MonocleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Monocle/PublicHeader.h>
+
+
